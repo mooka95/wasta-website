7418f34ee269f341cc9ffcf5ed6a68bd9495af7e
mooka95 <mohamed.osa.22@gmail.com>
2021-10-06 03:47:12 +0200

    fixing issues

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/myapp'));
+app.use(express.static(__dirname + '/dist/wasta-website'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/wasta-website/index.html'));});
